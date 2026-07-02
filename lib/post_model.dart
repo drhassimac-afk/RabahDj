50dfbac74af4fab1197f0de1a5a0a47bf4906fef
RabahDj <drhassimac@gmail.com>
2026-07-02 01:39:16 +0100

    Enhance PostModel with imageUrl and dateTime
    Added imageUrl and dateTime fields to PostModel, updated toMap and fromMap methods accordingly.

--- lib/post_model.dart
+++ lib/post_model.dart
@@ -4,25 +4,38 @@ class PostModel {
   final String id;
   final String username;
   final String content;
+  final String? imageUrl; // ميزة جديدة لدعم الصور داخل المنشور
   int likes;
+  final DateTime dateTime;
 
   PostModel({
     required this.id,
     required this.username,
     required this.content,
+    this.imageUrl,
     required this.likes,
+    required this.dateTime,
   });
 
   Map<String, dynamic> toMap() {
-    return {'id': id, 'username': username, 'content': content, 'likes': likes};
+    return {
+      'id': id,
+      'username': username,
+      'content': content,
+      'imageUrl': imageUrl,
+      'likes': likes,
+      'dateTime': dateTime.toIso8601String(),
+    };
   }
 
   factory PostModel.fromMap(Map<String, dynamic> map) {
     return PostModel(
       id: map['id'] ?? '',
-      username: map['username'] ?? 'RabahDj Local',
+      username: map['username'] ?? 'RabahDj User',
       content: map['content'] ?? '',
+      imageUrl: map['imageUrl'],
       likes: map['likes'] ?? 0,
+      dateTime: DateTime.parse(map['dateTime'] ?? DateTime.now().toIso8601String()),
     );
   }
 
