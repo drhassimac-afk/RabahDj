bd4b3c67fcb5c36f1dfd4c31b06e3bcca1f528c1
drhassimac-afk <drhassimac@example.com>
2026-08-01 23:11:15 +0100

    fix: Home screen overflow (ScrollView flex), add Files/Nearby tiles and screen

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import V2GamesScreen from './src/screens/v2/V2GamesScreen';
 import FileShareScreen from './src/screens/v2/FileShareScreen';
 import V2NotificationsScreen from './src/screens/v2/V2NotificationsScreen';
 import V2NearbyScreen from './src/screens/v2/V2NearbyScreen';
+import V2NearbyScreen from './src/screens/v2/V2NearbyScreen';
 
 const Stack = createStackNavigator();
 
@@ -34,6 +35,7 @@ export default function App() {
           <Stack.Screen name="Cinema" component={V2CinemaScreen} />
           <Stack.Screen name="Games" component={V2GamesScreen} />
           <Stack.Screen name="Files" component={FileShareScreen} />
+          <Stack.Screen name="Nearby" component={V2NearbyScreen} />
           <Stack.Screen name="Nearby" component={V2NearbyScreen} />
         </Stack.Navigator>
       </NavigationContainer>
