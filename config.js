49952e9409ebcb7a2d91aba4a4fe74f200cee534
drhassimac-afk <drhassimac@example.com>
2026-08-28 14:33:37 +0100

    إضافة: index.js, config.js وتحديث التبعيات

---
+++ config.js
@@ -0,0 +1,10 @@
+// تكوينات التطبيق
+export const SERVER_URL = 'http://192.168.100.2:4000';
+
+export const CONFIG = {
+  SERVER_URL,
+  APP_NAME: 'RabahDj',
+  VERSION: '1.0.8',
+};
+
+export default CONFIG;
